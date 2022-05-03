d3c66d7b5bfdf25fb5dd88ec97ecbc15f631273a
Karlitoyo <karltimmins@gmail.com>
2022-05-04 00:57:57 +0100

    fixed header issue for mobile view

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,68 +15,45 @@ const Header = () => {
     }
 
     return (
-        <header>
-            <nav className="bg-gradient-to-br from-red-500 to-purple-400 relative flex items-center justify-between h-16 font-poppins">
-                <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                    <div className="relative flex items-center justify-between h-16">
-                        <div className="absolute inset-y-0 left-0 flex sm:hidden">
+        <header className="h-24 flex items-center z-30 w-full bg-gradient-to-br from-red-500 to-purple-400">
+            <div className="container mx-auto px-6 flex items-center justify-between">
+                <div className="flex items-center">
 
-                            <button onClick={toggle} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                                <span className="sr-only">Open main menu</span>
+                    <nav className="font-poppins text-gray-800 dark:text-white uppercase text-lg lg:flex md:flex items-center hidden">
 
-                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                                </svg>
+                        <Link href="/"><a className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${currentRoute === '/' ? 'bg-gray-900' : ''}`}>Home</a></Link>{' '}{' '}
 
-                                <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
-                        </div>
-                        <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                            <div className="hidden sm:block sm:ml-6">
-                                <div className="flex space-x-4">
+                        <Link href="/marketplace"><a className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${currentRoute === '/marketplace' ? 'bg-gray-900' : ''}`}>Marketplace</a></Link>{' '}{' '}
 
-                                    <Link href="/"><a className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${currentRoute === '/' ? 'bg-gray-900' : ''}`}>Home</a></Link>{' '}{' '}
+                        <Link href="/about"><a className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${currentRoute === '/index' ? 'bg-gray-900' : ''}`}>Profile</a></Link>{' '}{' '}
 
-                                    <Link href="/marketplace"><a className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${currentRoute === '/marketplace' ? 'bg-gray-900' : ''}`}>Marketplace</a></Link>{' '}{' '}
+                    </nav>
 
-                                    <Link href="/about"><a className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${currentRoute === '/index' ? 'bg-gray-900' : ''}`}>Profile</a></Link>{' '}{' '}
+                    <button onClick={toggle} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white md:hidden lg:hidden" aria-controls="mobile-menu" aria-expanded="false">
 
-                                    {/* <Link href="/api/users"><a className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${currentRoute === '//api/users' ? 'bg-gray-900' : ''}`}>Settings</a></Link>{' '}{' '} */}
+                        <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
 
-                                </div>
+                        <div className="sm:hidden" id="mobile-menu">
+                            <div className={collapse ? "sm:flex sm:pt-0 w-full sm:w-auto px-2 pt-2 pb-3 space-y-1" : "hidden sm:flex"}>
+
+                                <Link href="/"><a className="font-medium text-gray-300 hover:text-gray-900">Home</a></Link>{' '}{' '}
+
+                                <Link href="/marketplace"><a className="font-medium text-gray-300 hover:text-gray-900">marketplace</a></Link>{' '}{' '}
+
+                                <Link href="/index"><a className="font-medium text-gray-300 hover:text-gray-900">Profile</a></Link>{' '}{' '}
+
                             </div>
                         </div>
-                        <div className='justify-end ml-72 -mr-96'>
-                            <Wallet />
-                        </div>
-                        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                            <div className="ml-3 relative">
-                                <div>
-                                    <button type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                        <span className="sr-only">Open user menu</span>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    </button>
                 </div>
-
-                <div className="sm:hidden" id="mobile-menu">
-                    <div className={collapse ? "sm:flex sm:pt-0 w-full sm:w-auto px-2 pt-2 pb-3 space-y-1" : "hidden sm:flex"}>
-
-                        <Link href="/"><a className="font-medium text-gray-300 hover:text-gray-900">Home</a></Link>{' '}{' '}
-
-                        <Link href="/marketplace"><a className="font-medium text-gray-300 hover:text-gray-900">marketplace</a></Link>{' '}{' '}
-
-                        <Link href="/index"><a className="font-medium text-gray-300 hover:text-gray-900">Profile</a></Link>{' '}{' '}
-
-                        {/* <Link href="/api/users"><a className="font-medium text-gray-300 hover:text-gray-900">Settings</a></Link>{' '}{' '} */}
-                    </div>
+                <div className="uppercase text-gray-800 dark:text-white font-black text-3xl flex items-center">
+                    <span className="text-xs ml-3 mt-1">
+                        <Wallet />
+                    </span>
                 </div>
-            </nav>
-
+            </div>
         </header>
     )
 }
