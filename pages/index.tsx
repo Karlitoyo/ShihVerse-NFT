6570789b3d1cd05ea34af54ad9276a103df1cf33
Karlitoyo <karltimmins@gmail.com>
2022-05-04 01:48:57 +0100

    amended picture sizing and position for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,13 +9,14 @@ const IndexPage = () => {
 
   return (
     <div className="bg-gradient-to-tr from-red-500 to-purple-400 relative h-screen overflow-hidden">
-      <Image src={Shihverse}
-        // loader={myLoader}
-        className="absolute inset-0 w-full h-full object-cover filter brightness-50"
-        alt="main background image"
-      ></Image>
-
-      <div className="absolute inset-0 flex flex-col justify-center items-center w-5/6 max-w-lg mx-auto mr-60 mb-28 text-center">
+      <div className='md:flex md:justify-center lg:justify-left lg:absolute'>
+        <Image src={Shihverse}
+          // loader={myLoader}
+          className="sm:relative lg:absolute md:absolute inset-0 w-full md:justify-center h-full object-cover filter brightness-50"
+          alt="main background image"
+        ></Image>
+      </div>
+      <div className="lg:absolute md:relative inset-0 flex flex-col justify-center items-center w-5/6 max-w-lg mx-auto lg:mr-60 md:-mt-28 text-center overflow-hidden">
 
         <h1 className="font-primary font-extrabold text-white text-3xl sm:text-4xl md:text-5xl md:leading-snug">
           SHIHverse NFT
